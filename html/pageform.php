1f1d506224bd717c22750b63246bc500ed641fb9
Gonzalo Ramirez <gonzaloramirez.sagitario@gmail.com>
2022-07-18 14:06:22 -0300

    -version 1.0

---
+++ html/pageform.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/c8757c1678.js" crossorigin="anonymous"></script> 
+    <link rel="stylesheet" href="../../infotkd_web_site/css/normalize.css">
+    <link rel="stylesheet" href="../../infotkd_web_site/css/stylepageform.css">
+    <title>Infotkd</title>
+</head>
+<body>
+    <header class="nav__background">
+        <nav class="nav__contenedor container">
+
+            <div class="nav__logo logo__order">
+                <a class="nav-logo-png" href="../../infotkd_web_site/html/index.html">
+                    <img src="../../infotkd_web_site/image/infotkd-logo.png" >
+                </a>
+            </div>
+
+            <div class="cont_links" id="cont_links">
+                
+                <ul class="nav__link nav__order">
+                    <li class="nav__items">
+                        <a href="../../infotkd_web_site/html/index.html" class="nav__links"><i class="fa-solid fa-house"></i>Home</a>
+                    </li>
+
+                    <li class="nav__items">
+                        <a href="../../infotkd_web_site/html/pagina_en_construccion.html" class="nav__links"><i class="fa-solid fa-newspaper"></i>Noticias</a>
+                    </li>
+
+                    <li class="nav__items">
+                        <a href="../../infotkd_web_site/html/pagina_en_construccion.html" class="nav__links"><i class="fa-solid fa-calendar-check"></i>Eventos</a>
+                    </li>
+                    
+                    <li class="nav__items">
+                        <a href="../../infotkd_web_site/html/pagina_en_construccion.html" class="nav__links"><i class="fa-solid fa-barcode"></i>BBL</a>
+                    </li>
+
+                    <li class="nav__items">
+                        <a href="../../infotkd_web_site/html/pagina_en_construccion.html" class="nav__links"><i class="fa-solid fa-paper-plane"></i>Contacto</a>
+                    </li>
+                </ul> 
+            </div>  
+            <div id="menu_hamburguesa">
+                <i class="fa-solid fa-bars"></i>
+            </div>  
+        </nav>
+    </header>
+
+
+    <div class="background_form1" id="move_background_form1">
+        <span class="tittle_form"><h1>Torneo Nacional 2023- Inscripciones</h1></span>
+
+        <?php if(!empty($error)): ?>
+            <div class="div_alerta">
+                <div class="div_alerta_icon">
+                    <i class="fa-solid fa-triangle-exclamation"></i>
+                </div>
+                <div class="div_alerta_p"><?php echo $error; ?></div>
+            </div>
+        <?php endif; ?>
+
+        <?php if(!empty($exitoso)): ?>
+            <div class="div_exitoso">
+                <div class="div_exitoso_icon">
+                    <i class="fa-solid fa-circle-check"></i>
+                </div>
+                <div class="div_exitoso_p"><?php echo $exitoso; ?></div>
+            </div>
+        <?php endif; ?>
+
+        <?php if(!empty($alerta_dni)): ?>
+            <div class="div_alerta_dni">
+                <div class="div_alerta_dni_p"><?php echo $alerta_dni; ?></div>
+            </div>
+        <?php endif; ?>
+
+        <form name="form1" action="../../infotkd_web_site/php/form.php" method="POST">
+            <div class="container_form">
+                <div class="rows_inputsA">
+                    <div class="contenedor_spanTittleRow">
+                        <span><h3>Datos del Coach/Instructor</h3></span>
+                    </div>
+
+                    <div class="columns_inputA">
+                        <div class="div_inputsA">
+                            <input type="text" name="escuela" id="" placeholder="Escuela" require>
+                        </div>
+    
+                        <div class="div_inputsA">
+                            <input type="text" name="instructor" id="" placeholder="Instructor (Nombre y Apellido)" require> 
+                        </div>
+    
+                        <div class="div_inputsA">
+                            <input type="text" name="coach" id="" placeholder="Coach (Nombre y Apellido)" require>
+                        </div>
+                 
+                        <div class="div_inputsA">
+                            <input type="text" name="email" id="" placeholder="E-mail: xxxxxxx@gmail.com" require>
+                        </div>
+    
+                        <div class="div_inputsA">
+                            <input type="tel" name="celular" id="" placeholder="Celular/Whatsapp" require>
+                        </div>
+
+                        <div class="div_inputsA">
+                            <input type="text" name="othercamp" id="" placeholder="Othercamp">
+                        </div>
+                    </div>
+                </div>
+                
+    
+                <div class="rows_inputsB clonar">
+                    <div class="columns_inputsB">
+                        <div class="contenedor_spanTittleRowB">
+                            <h3>Datos del Competidor</h3>
+                            <span class="puntero ocultar">Eliminar</span>
+                        </div>
+
+                        <div class="contenedor_nombre_dni">
+                            <div class="div_inputsB div_inputsB_competidor">
+                                <input type="text" name="competidor[]" id="" placeholder="Nombre y Apellido del competidor" require>
+                            </div>
+    
+                            <div class="div_inputsB div_inputsB_dni">
+                                <input type="tel" name="dni[]" id="" placeholder="DNI (sin puntos ni comas)" require>
+                            </div>
+                        </div>
+
+                        <div class="div_inputsB div_genero">
+                            <span><p>Género</p></span>
+                            <select name="genero[]" id="">
+                                <option value="-">-</option>
+                                <option value="M">Masculino</option>
+                                <option value="F">Femenino</option>
+                            </select>  
+                        </div>
+        
+                        <div class="div_inputsB div_categoria">
+                            <span><p>Categoría</p></span>
+                            <select name="categoria[]" id="">
+                                <option value="-">-</option>
+                                <option value="Blanco - Blanco P. Amarilla">Blanco - Blanco P. Amarilla</option>
+                                <option value="Verde - Verde P. Azul">Verde - Verde P. Azul</option>  
+                                <option value="Amarillo - Amarillo P. Verde">Amarillo - Amarillo P. Verde</option>
+                                <option value="Azul - Azul P. Roja">Azul - Azul P. Roja</option>
+                                <option value="Rojo - Rojo P. Negra">Rojo - Rojo P. Negra</option>
+                                <option value="1° Dan">1° Dan</option>
+                                <option value="2° Dan">2° Dan</option>
+                                <option value="3° Dan">3° Dan</option>
+                                <option value="4° Dan">4° Dan</option>
+                                <option value="5° Dan">5° Dan</option>
+                                <option value="6° Dan">6° Dan</option>
+                                <option value="7° Dan">7° Dan</option>
+                                <option value="8° Dan">8° Dan</option>
+                                <option value="9° Dan">9° Dan</option>
+                            </select>
+                        </div> 
+                    
+                        <div class="div_inputsB div_edad">
+                            <span><p>Edad</p></span>
+                            <select name="edad[]" id="inputEdad">
+                                <option value="-">-</option>
+                                <option value="2-5 años">2 a 5 años</option>
+                                <option value="6-9 años">6 a 9 años</option>
+                                <option value="10-13 años">10 a 13 años</option>
+                                <option value="14-17 años">14 a 17 años</option>
+                                <option value="18-35 años">18 a 35 años</option>
+                                <option value="+35 años">+35 años</option>
+                            </select>                        
+                        </div>
+        
+                        <div class="div_inputsB div_peso">
+                            <span><p>Peso*</p></span>
+                            <select name="peso[]" id="">
+                                <option value="-">-</option>
+                                <option value="60-65kg">60-65kg</option>
+                                <option value="66-70kg">66-70kg</option>
+                                <option value="71-80kg">71-80kg</option>
+                                <option value="+80kg">+80kg</option>
+                                <option value="No Aplica">No Aplica</option>
+                            </select>
+                        </div> 
+                        <span class="leyendaNoAplica"><p>*Tenga en cuenta que aquellos competidores menores a 13 años
+                            deben elegir la opción 'No Aplica' para su peso.
+                        </p></span>
+                    </div>
+                </div>
+            </div>
+            <div class="contenedorClonado" id="contenedorClonado"></div>
+
+            <div class="div_buttons" id="div_btn_agregar">
+                <button class="button_agregar" id="agregar">Agregar competidor</button>
+                <button class="button_enviar" id="enviar" name="enviar">Enviar</button>
+            </div>
+        </form>
+    </div>
+
+
+    
+
+
+    <script>
+        let agregar = document.getElementById("agregar");
+        let contenido = document.getElementById("contenedorClonado");
+
+        agregar.addEventListener("click", e =>{
+            e.preventDefault();
+
+            let clonado = document.querySelector(".clonar");
+            let clon = clonado.cloneNode(true);
+
+            contenido.appendChild(clon).classList.remove("clonar");
+
+            let remover = contenido.lastChild.childNodes[1].querySelectorAll("span");
+            remover[0].classList.remove("ocultar");
+        });
+
+        contenido.addEventListener("click", e=>{
+            e.preventDefault();
+
+            if(e.target.classList.contains("puntero")){
+
+                let contenedor = e.target.parentNode.parentNode.parentNode;
+
+                contenedor.parentNode.removeChild(contenedor);
+            }
+        });
+    </script>
+
+    <!-- Función mostrar menú -->
+
+    <script>
+        document.getElementById("menu_hamburguesa").addEventListener("click", mostrarMenu);
+
+        function mostrarMenu (){
+            document.getElementById("move_background_form1").classList.toggle('move_background_form1');
+            document.getElementById("cont_links").classList.toggle('move_cont_links');
+        };
+    </script>
+
+    <!-- <script src="../infotkd_web_site/js/script.js"></script> -->
+
+</body>
+</html>
---
+++ php/form.php
@@ -0,0 +1,97 @@
+<?php
+$usuario        = "root";
+$password       = "";
+$servidor       = "localhost";
+$basededatos    = "circuitofschool";
+$con            = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
+$db             = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
+
+$escuela = $_POST['escuela'];
+$instructor = $_POST['instructor'];
+$coach = $_POST['coach'];
+$email = $_POST['email'];
+$celular = $_POST['celular'];
+$othercamp = $_POST['othercamp'];
+$competidor = $_POST['competidor'];
+$dni = $_POST['dni'];
+$genero = $_POST['genero'];
+$categoria = $_POST['categoria'];
+$edad = $_POST['edad'];
+$peso = $_POST['peso'];
+$error = '';
+$exitoso = '';
+$alerta_dni = '';
+
+if(empty($escuela) or empty($instructor) or empty($coach) or empty($email) or empty ($celular)
+or empty($competidor) or empty($dni) or empty($genero) or empty($categoria) or empty($edad)
+or empty($peso)){
+
+    $error .= '<i>¡Se deben completar todos los campos!</i>';
+    require '../../infotkd_web_site/html/pageform.php';
+
+}else{
+
+    for ($i=0; $i < count($competidor); $i++){
+
+        $InserData =("INSERT INTO competidores (escuela, instructor, coach, email, celular, othercamp,
+        competidor, dni, genero, categoria, edad, peso) VALUES ('".$escuela."','".$instructor."','".$coach."','".$email."','".$celular."',
+        '".$othercamp."','".$competidor[$i]."','".$dni[$i]."','".$genero[$i]."','".$categoria[$i]."','".$edad[$i]."','".$peso[$i]."')");
+        
+        
+        //verificamos que el dni ingresado no haya sido ingresado//
+        //creamos la variable que hara la consulta en la base de datos//
+        //si el dni se repite, este se omite y se continua con la carga de datos//
+        
+        $verificador_dni = mysqli_query($con, "SELECT * FROM competidores WHERE dni='".$dni[$i]."' ");
+        
+        if(mysqli_num_rows($verificador_dni) > 0){   
+            $alerta_dni .= '<i>¡El DNI ingresado ya se encuentra registrado! Contáctese a infotkd@gmail.com</i>';
+            require '../../infotkd_web_site/html/pageform.php';
+            continue;
+        }else{
+            $ejecutar = mysqli_query($con, $InserData);
+
+            if($ejecutar){
+                $exitoso .= '<i>¡Todos los datos se han enviado exitosamente!</i>';
+                require '../../infotkd_web_site/html/pageform.php';
+            };
+            
+        };
+        
+    };
+        
+    mysqli_close($con);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Validación: verificamos que $ejecutar se haya ejecutado y enviado los datos*/
+/*if($ejecutar){
+    echo '
+        <script>
+            alert("¡Los datos han sido enviados con éxito!");
+            window.location = "../../infotkd_web_site/html/pageform.html";
+        </script>
+    ';
+}else{
+    echo '
+        <script>
+            alert("¡Inténtalo de nuevo, algo ha fallado!");
+            window.location = "../../infotkd_web_site/html/pageform.html";
+        </script>
+    ';
+}*/
+
+/*header('Location: ./');*/
+?>
