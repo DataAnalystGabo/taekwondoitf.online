5a6b31df2f126bb00e45cd74f73c19e86c40eaaa
Gonzalo Ramirez <gonzaloramirez.sagitario@gmail.com>
2022-09-19 22:31:17 -0300

    eliminando archivos

--- js/ventanaModal.js
+++
@@ -1,5 +0,0 @@
-const aceptarModal = document.getElementById("modal__boton");
-const contModal = document.getElementById("cont_modal");
-const ventanaModal = document.getElementById("ventana_modal");
-
-aceptarModal.addEventListener("click", () => contModal.style.display  = "none");
