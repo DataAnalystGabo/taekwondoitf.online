e1de3ed601679cfbabceee94d4449e185c69c144
Gonzalo Ramirez <gonzaloramirez.sagitario@gmail.com>
2022-10-19 18:15:06 -0300

    -v 1.2.6 rediseño del header

--- index.php
+++ index.php
@@ -43,33 +43,27 @@ $statementSQL->setFetchMode(PDO::FETCH_ASSOC);
 
                 <div class="menu" id="menu">
                     <div class="menu__contenedor">
-                        <h3 class="menu__titulo">infotkd</h3>
                         <ul class="menu__links">
                             <li class="menu__li">
-                                <a href="./index.php" class="menu__a">Home<i class="menu__i fa-solid fa-house"></i></a>
+                                <a href="./index.php" class="menu__a">HOME</a>
                             </li>
 
                             <li class="menu__li">
-                                <a href="pages/news.php" class="menu__a">Noticias<i class="menu__i fa-solid fa-newspaper"></i></a>
+                                <a href="pages/news.php" class="menu__a">NOTICIAS</a>
                             </li>
 
                             <li class="menu__li">
-                                <a href="pages/404.html" target="_blank" class="menu__a">Eventos<i class="menu__i fa-solid fa-calendar-check"></i></a>
+                                <a href="pages/404.html" target="_blank" class="menu__a">EVENTOS</a>
                             </li>
                             
                             <li class="menu__li">
-                                <a href="pages/404.html" target="_blank" class="menu__a">BBL<i class="menu__i fa-solid fa-barcode"></i></a>
+                                <a href="pages/404.html" target="_blank" class="menu__a">BLACK BELT LIST</a>
                             </li>
 
                             <li class="menu__li">
-                                <a href="pages/contact.php" class="menu__a">Contacto<i class="menu__i fa-solid fa-paper-plane"></i></a>
+                                <a href="pages/contact.php" class="menu__a">CONTACTO</a>
                             </li>
                         </ul>
-                        <div class="redes">
-                            <a class="redes__link" href="#" title="Facebook"><i class="redes__i fa-brands fa-facebook"></i></a>
-                            <a class="redes__link" href="#" title="Instagram"><i class="redes__i fa-brands fa-instagram"></i></a>
-                            <a class="redes__link" href="#" title="Twitter"><i class="redes__i fa-brands fa-twitter"></i></a>
-                        </div>
                     </div> 
                 </div>  
 
