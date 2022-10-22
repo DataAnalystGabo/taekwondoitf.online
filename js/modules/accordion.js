c076eff123349762152d88d32c08800951c2d937
Gonzalo Ramirez <gonzaloramirez.sagitario@gmail.com>
2022-10-22 00:11:40 -0300

    -v 1.3.0 creacion de teorias.php

---
+++ js/modules/accordion.js
@@ -0,0 +1,6 @@
+const more = document.querySelector('.accordion__more');
+
+
+more.addEventListener('click', ()=>{
+    more.classList.toggle('accordion__more--active');
+});
---
+++ pages/teorias.php
@@ -0,0 +1,79 @@
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>▷ Taekwondo ITF - ¡Todo lo que tenés que saber está aquí!</title>
+        <meta name="description" content="Leé noticias, Enterate de los eventos deportivos, Descargá teoría en pdf, Verificá si un instructor está afederado y mucho más.">
+        <meta name="keyword" content="Taekwondo, ITF, Eventos, Cinturon negro, Dobok">
+        
+        <script defer src="https://kit.fontawesome.com/c8757c1678.js" crossorigin="anonymous"></script> 
+        <script type="module">
+            import { menu } from '../js/modules/menu.js';
+        </script>
+        <script defer src="../js/modules/accordion.js"></script>
+        <script defer src="https://www.googletagmanager.com/gtag/js?id=G-0PWZ0SZ5KK"></script>
+        <script defer> 
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0PWZ0SZ5KK');
+        </script>
+
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/root.css">
+        <link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="../css/teorias.css">
+        <link rel="stylesheet" href="../css/footer.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,700;1,900&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap">
+    </head>
+
+    <body>
+        <?php require('../layout/header.php') ?>
+
+        <main class="main container">
+            <div class="intro">
+                <h2 class="intro__title">TEORIAS</h2>
+                <p class="intro__paragraph">
+                    A continuación, se presenta una guía teórica para el estudio del 
+                    Taekwono ITF dividida en dos categorías : -13 años y +13 años.
+                    Tenga en cuenta que el contenido que verá es una adaptación y recopilación 
+                    de fuentes como -fuentes- . Los contenidos son una sugerencia que no pretende 
+                    mostrarse como una guía definitiva. Más bien, trata de acercar al practicante de 
+                    Taekwondo ITF las bases para el correcto estudio.
+                </p>
+                <p class="intro__paragraph">
+                    Es posible que el contenido difiera del de otros autores o recursos ya que, cada 
+                    línea técnica y/o escuela suele adaptar o presentar la información según sus 
+                    criterios y requirimientos.
+                </p>
+            </div>
+
+            <div class="accordion">
+                <div class="accordion__card">
+                    <picture class="accordion__cover">                    
+                        <img class="accordion__img" 
+                            srcset="../image/accordion-1-mobile.webp 435w"
+
+                            sizes="(max-width : 500px) 435px"
+
+                            src="image/cover-prime-480x1000.webp" 
+                            alt="Dos competidores de Taekwondo ITF enfrentandose en un torneo">
+                    </picture>
+                    <div class="accordion__filter"></div>
+                    <div class="accordion__textarea">
+                        <span class="accordion__title">-13 AÑOS</span>
+                        <span class="accordion__more">+</span>  
+                    </div>                    
+                </div>
+            </div>
+        </main>
+        
+    </body>
+</html>
